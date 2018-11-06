4faf8813b3ae79bda4d16162b75da0379f36fd38
Basia <bbujnik@wp.pl>
2018-11-07 00:34:59 +0100

    added method for getting numbers from user

---
+++ src/pl/coderslab/zad_2/Application.java
@@ -0,0 +1,12 @@
+package pl.coderslab.zad_2;
+
+import static pl.coderslab.zad_2.Lotto.lotto;
+
+
+public class Application {
+    public static void main(String[] args) {
+
+        lotto();
+
+    }
+}
--- src/pl/coderslab/zad_2/Lotto.java
+++ src/pl/coderslab/zad_2/Lotto.java
@@ -1,51 +1,86 @@
-//package pl.coderslab.zad_2;
+package pl.coderslab.zad_2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lotto {
+
+    // zadanie nieskończone
+
+    public static void lotto() {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int min = 1;
+        int max = 49;
+        int amountNumbersToCompare = 6;
+
+        Integer[] arr = new Integer[max];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        // System.out.println(Arrays.toString(arr)); // tablica liczb od 1 do 49
+        Collections.shuffle(Arrays.asList(arr));  // miesza liczby
+        // System.out.println(Arrays.toString(arr)); // tablica z pomieszanymi liczbami
+
+        // trzeba wylosować 6, które potem porównać z wyboranymi przez użytkownika (amountNumberToCompare)
+        Random rand = new Random();
+        int tabRand[] = new int[amountNumbersToCompare];
+        for (int j = 0; j < tabRand.length; j++) {
+            tabRand[j] = rand.nextInt(max+1) + min; // randomowe liczby z zakresu 1-49 (max=49)
+        }
+        // System.out.println(Arrays.toString(tabRand)); // tablica wylosowanych 6 liczb
+
+
+        System.out.println("Witaj w losowaniu Lotto. Musisz wytypować "+amountNumbersToCompare+" liczb");
+        System.out.println("Wytypuj liczby z zakresu "+min+" - "+max+" do losowania: ");
+        getNumber(scanner);
+
 //
-//import java.util.Arrays;
-//import java.util.Collections;
-//import java.util.Random;
-//import java.util.Scanner;
-//
-//public class Lotto {
-//
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//        lotto(scanner);
-//        scanner.close();
-//
-//    }
-//
-//    private static void lotto(Scanner scanner) {
-//
-//        Integer[] arr = new	Integer[49];
-//        for	(int i = 0;	i <	arr.length;	i++)	{
-//            arr[i] = i + 1;
-//        }
-//        System.out.println(Arrays.toString(arr)); // tablica liczb od 1 do 49
-//        Collections.shuffle(Arrays.asList(arr));  // miesza liczby
-//        System.out.println(Arrays.toString(arr)); // tablica z pomieszanymi liczbami
-//
-//        // trzeba wylosować 6, które potem porównać z wyboranymi przez użytkownika
-//        Random rand = new Random();
-//        int tabRand []= new int [6];
-//        for (int j=0; j<tabRand.length; j++) {
-//            tabRand [j] = rand.nextInt(49);
-//        }
-//        System.out.println(Arrays.toString(tabRand)); // ??
-//
-//        while (true) {
-//            System.out.println("Wytypuj pierwszą liczbę z zakresu 1-49 do losowania Lotto: ");
-//
-//        }
-//        int choice = scanner.nextInt();
-//        if (choice >= 1 && choice <= 49) {
-//            // tu instrukcje na sprawdzenie czy podana liczba się nie powtórzyła
-//
-//        } else {
-//            System.out.println("Podałeś liczbe nie z zakresu 1-49. Spróbuj ponownie");
-//        }
-//
-//
-//
-//    }
-//}
+//        System.out.println("Dziękujemy! Sprawdźmy czy wygrana jest Twoja");
+
+        scanner.close();
+
+        // spr czy 3,4,5,6 podanych liczb jest takich samych jak wylosowane, true -> wygrana
+
+    }
+
+    // metoda na pobranie liczb od użytkownika
+    public static void getNumber(Scanner scanner) {
+
+        int min = 1;
+        int max = 49;
+
+        int amountNumbersToCompare = 6;
+
+        while (!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.out.println("Musisz podać liczbę. Spróbuj jeszcze raz!");
+        }
+
+        int choice = scanner.nextInt();
+        if (choice < min && choice > max) {
+            System.out.println("Podałeś liczbe spoza zakresu "+min+" - "+max+". Spróbuj ponownie");
+        } else {
+            // tu instrukcje na sprawdzenie czy podana liczba się nie powtórzyła
+            // podane przez uzytkownika liczby zapisane do tabeli
+            for (int i = 0; i < amountNumbersToCompare; i++) {
+                int [] choiceTab = new int[amountNumbersToCompare];
+
+/*
+pomyśleć nad tym, nie tak
+                    if (choice != choiceTab[i]) {
+                    choiceTab [i] = scanner.nextInt();
+                    scanner.nextLine();
+                }
+                System.out.println(Arrays.toString(choiceTab));
+*/
+            }
+        }
+
+    }
+
+
+}
